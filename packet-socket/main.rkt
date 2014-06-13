ef7fb958c4c21725ed787e3340a64a9e7ec48f2c
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-06-13 19:50:42 -0400

    OSX BPF implementation

--- packet-socket/main.rkt
+++ packet-socket/main.rkt
@@ -8,34 +8,61 @@
 	 raw-interface-read
 	 raw-interface-write)
 
+(require racket/match)
+(require (only-in racket/list remove-duplicates))
 (require "private/packet-socket-extension.rkt")
 
-(struct raw-interface (fd) #:transparent)
+(struct raw-interface (name [fd #:mutable] buflen [buffered-packets #:mutable]) #:transparent)
 
 ;; -> (Option (Listof String))
 ;; #f if the platform doesn't support it.
 (define (raw-interface-names)
-  (enumerate-interfaces))
+  (define names (enumerate-interfaces))
+  (and names (remove-duplicates names)))
+
+;; Handle -> Void
+;; Errors if the handle is closed
+(define (check-open f handle)
+  (unless (number? (raw-interface-fd handle))
+    (error f "Attempt to use closed handle ~v" handle)))
 
 ;; String -> (Option Handle)
 ;; #f if the platform doesn't support it, the interface can't be
 ;; found, or some other error occurs.
 (define (raw-interface-open interface-name)
-  (define fd (create-and-bind-socket (string->bytes/utf-8 interface-name)))
-  (and fd (raw-interface fd)))
+  (match (create-and-bind-socket (string->bytes/utf-8 interface-name))
+    [#f #f]
+    [fd (match (socket-read-buffer-length fd)
+	  [-1 (begin (socket-close fd) #f)]
+	  [buflen (raw-interface interface-name
+				 fd
+				 buflen
+				 '())])]))
 
 ;; Handle -> Boolean
 (define (raw-interface-close handle)
-  (close-socket (raw-interface-fd handle)))
+  (check-open 'raw-interface-close handle)
+  (socket-close (raw-interface-fd handle))
+  (set-raw-interface-fd! handle #f))
 
 ;; Handle -> (Option Bytes)
 ;; #f if the platform doesn't support it.
 (define (raw-interface-read handle)
-  (define buffer (make-bytes 1522)) ;; Ethernet frame size
-  (define len (socket-read (raw-interface-fd handle) buffer))
-  (subbytes buffer 0 len))
+  (match (raw-interface-buffered-packets handle)
+    ['()
+     (define buffer (make-bytes (raw-interface-buflen handle)))
+     (define subpacketspecs (socket-read (raw-interface-fd handle) buffer))
+     (define buffered-packets (for/list [(subpacketspec subpacketspecs)]
+				(match-define (cons offset len) subpacketspec)
+				(subbytes buffer offset (+ offset len))))
+     (set-raw-interface-buffered-packets! handle buffered-packets)
+     (raw-interface-read handle)]
+    [(cons packet rest)
+     (set-raw-interface-buffered-packets! handle rest)
+     packet]))
 
 ;; Handle Bytes -> Void
 ;; #f if the platform doesn't support it.
 (define (raw-interface-write handle bs)
+  (check-open 'raw-interface-write handle)
   (socket-write (raw-interface-fd handle) bs))
--- packet-socket/private/packet-socket-extension.c
+++ packet-socket/private/packet-socket-extension.c
@@ -1,49 +1,65 @@
-#ifdef __linux__
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+
+#include <fcntl.h>
+#include <err.h>
+#include <errno.h>
+#include <unistd.h>
+#include <ifaddrs.h>
 
 #include <net/if.h>
-#include <ifaddrs.h>
-#include <netpacket/packet.h>
 #include <net/ethernet.h>
+
+#if defined(__linux__)
+#include <netpacket/packet.h>
+#endif
+
+#if defined(__APPLE__)
+#include <net/bpf.h>
 #endif
 
 #include "escheme.h"
 
-#ifdef __linux__
+/***************************************************************************/
+/* Linux-specific implementation */
 
-int getInterfaceIndexInternal(int sock, char* interfaceName) {
-  struct ifreq *ifr;
+#if defined(__linux__)
+
+static int getInterfaceIndexInternal(int sock, char const *interfaceName)
+  XFORM_SKIP_PROC
+{
+  struct ifreq ifr;
   int index_result;
 
-  ifr = (struct ifreq*) malloc(sizeof(struct ifreq));
-  strncpy((char *) ifr->ifr_name, interfaceName, IFNAMSIZ);
+  strncpy(ifr.ifr_name, interfaceName, IFNAMSIZ);
   /* printf("The interface for index: %s\n", ifr->ifr_name); */
 
-  if (ioctl(sock, SIOCGIFINDEX, ifr) < 0) {
+  if (ioctl(sock, SIOCGIFINDEX, &ifr) < 0) {
     perror("ioctl error while looking up interface");
     return -1;
   }
 
-  index_result = ifr->ifr_ifindex;
-  free(ifr);
-  return index_result;
+  return ifr.ifr_ifindex;
 }
 
-
-int bindToInterface(int sock, char* interfaceName, int index) {
+static int bindToInterface(int sock, char const *interfaceName)
+  XFORM_SKIP_PROC
+{
   struct sockaddr_ll socketAddress;
-
   socketAddress.sll_family = AF_PACKET;
-  socketAddress.sll_ifindex = index;
   socketAddress.sll_protocol = htons(ETH_P_ALL);
+  socketAddress.sll_ifindex = getInterfaceIndexInternal(sock, interfaceName);
+  if (socketAddress.sll_ifindex == -1) {
+    return -1;
+  }
 
-  if (bind(sock, (struct sockaddr *) &socketAddress, sizeof(struct sockaddr_ll)) < 0) {
+  if (bind(sock, (struct sockaddr *) &socketAddress, sizeof(socketAddress)) < 0) {
     perror("Bind error");
     return -1;
   }
@@ -51,22 +67,158 @@ int bindToInterface(int sock, char* interfaceName, int index) {
   return 0;
 }
 
-void setPromiscuousMode(int sock, int index) {
-  struct packet_mreq *req;
-
-  req = (struct packet_mreq*)malloc(sizeof(struct packet_mreq));
-  req->mr_ifindex = index;
-  req->mr_type = PACKET_MR_PROMISC;
-
-  if (setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, (void*)req, (socklen_t)sizeof(struct packet_mreq)) < 0) {
-    perror("set sock opt error");
+static int openSocket(char const *interfaceName)
+  XFORM_SKIP_PROC
+{
+  int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+  if (sock < 0) {
+    perror("Socket error");
+    return -1;
   }
+
+  if (bindToInterface(sock, interfaceName) == -1) {
+    return -1;
+  }
+
+  return sock;
 }
 
-#endif /* def __linux__ */
+static int readBufferLength(int sock)
+  XFORM_SKIP_PROC
+{
+  /* TODO: larger? */
+  return ETHER_MAX_LEN;
+}
 
-Scheme_Object *enumerate_interfaces(int argc, Scheme_Object **argv) {
-#ifdef __linux__
+static int extractPacket(char * const bufbase, size_t limit, int o, int *basep, int *lenp) {
+  *basep = 0;
+  *lenp = (o == 0) ? limit : 0;
+  return limit;
+}
+
+/*
+static int setPromiscuousMode(int sock, int index)
+  XFORM_SKIP_PROC
+{
+  struct packet_mreq req;
+
+  req.mr_ifindex = index;
+  req.mr_type = PACKET_MR_PROMISC;
+
+  if (setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &req, (socklen_t) sizeof(req)) < 0) {
+    perror("set sock opt error");
+    return -1;
+  }
+
+  return 0;
+}
+*/
+
+#endif
+
+/***************************************************************************/
+/* OSX-specific implementation */
+#if defined(__APPLE__)
+
+static int openSocket(char const *interfaceName)
+  XFORM_SKIP_PROC
+{
+  int deviceIndex;
+
+  for (deviceIndex = 0; deviceIndex < 10; deviceIndex++) {
+    int fd = -1;
+
+    {
+      char deviceName[32];
+      snprintf(deviceName, sizeof(deviceName), "/dev/bpf%d", deviceIndex);
+      fd = open(deviceName, O_RDWR);
+      if (fd == -1) {
+	if (errno != ENOENT) {
+	  perror("open /dev/bpfX");
+	}
+	continue;
+      }
+    }
+
+    {
+      struct ifreq ifr;
+      strncpy(ifr.ifr_name, interfaceName, IFNAMSIZ);
+      if (ioctl(fd, BIOCSETIF, &ifr) < 0) {
+	perror("ioctl BIOCSETIF");
+	close(fd);
+	continue;
+      }
+    }
+
+    /* { */
+    /*   u_int enable = 1; */
+    /*   if (ioctl(fd, BIOCSHDRCMPLT, &enable) < 0) { */
+    /* 	perror("ioctl BIOCSHDRCMPLT"); */
+    /* 	close(fd); */
+    /* 	continue; */
+    /*   } */
+    /* } */
+
+    /* { */
+    /*   u_int enable = 1; */
+    /*   if (ioctl(fd, BIOCSSEESENT, &enable) < 0) { */
+    /* 	perror("ioctl BIOCSSEESENT"); */
+    /* 	close(fd); */
+    /* 	continue; */
+    /*   } */
+    /* } */
+
+    {
+      u_int enable = 1;
+      if (ioctl(fd, BIOCIMMEDIATE, &enable) < 0) {
+	perror("ioctl BIOCIMMEDIATE");
+	close(fd);
+	continue;
+      }
+    }
+
+    return fd;
+  }
+
+  return -1;
+}
+
+static int readBufferLength(int fd)
+  XFORM_SKIP_PROC
+{
+  u_int buflen;
+  if (ioctl(fd, BIOCGBLEN, &buflen) < 0) {
+    perror("ioctl BIOCGBLEN");
+    return -1;
+  }
+  return buflen;
+}
+
+static int extractPacket(char *bufbase, size_t limit, int o, int *basep, int *lenp)
+  XFORM_SKIP_PROC
+{
+  struct bpf_hdr *bh = (struct bpf_hdr *) (bufbase + o);
+  struct ether_header *eh = (struct ether_header *) (bufbase + o + bh->bh_hdrlen);
+  int nexto;
+  *basep = o + bh->bh_hdrlen;
+  *lenp = bh->bh_caplen;
+  if (bh->bh_caplen != bh->bh_datalen) {
+    fprintf(stderr, "packet-socket: Warning: packet truncated from %u to %u bytes\n",
+	    bh->bh_datalen,
+	    bh->bh_caplen);
+  }
+  nexto = o + BPF_WORDALIGN(bh->bh_hdrlen + bh->bh_caplen);
+  /* printf("base %p limit %u o %d *basep now %d *lenp now %d nexto %d\n", */
+  /* 	 bufbase, limit, o, *basep, *lenp, nexto); */
+  return nexto;
+}
+
+#endif
+
+/***************************************************************************/
+/* Common implementation */
+
+static Scheme_Object *enumerate_interfaces(int argc, Scheme_Object **argv) {
   Scheme_Object *result = scheme_null;
   struct ifaddrs *addrs;
   struct ifaddrs *tmp;
@@ -77,54 +229,23 @@ Scheme_Object *enumerate_interfaces(int argc, Scheme_Object **argv) {
   }
 
   for (tmp = addrs; tmp != NULL; tmp = tmp->ifa_next) {
-    if (tmp->ifa_addr != NULL && tmp->ifa_addr->sa_family == AF_PACKET) {
+    if (tmp->ifa_addr != NULL) {
       result = scheme_make_pair(scheme_make_utf8_string(tmp->ifa_name), result);
     }
   }
 
   freeifaddrs(addrs);
   return result;
-#endif
-
-#if defined(__APPLE__) && defined(__MACH__)
-  return scheme_false;
-#endif
 }
 
-Scheme_Object *create_and_bind_socket(int argc, Scheme_Object **argv) {
-#ifdef __linux__
-  int sock, index;
-  char *interface_name;
-
-  interface_name = SCHEME_BYTE_STR_VAL(argv[0]);
-
-  sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-  if (sock < 0) {
-    perror("Socket error");
-    return scheme_false;
-  }
-
-  index = getInterfaceIndexInternal(sock, interface_name);
-  if (index == -1) {
-    return scheme_false;
-  }
-
-  if (bindToInterface(sock, interface_name, index) == -1) {
-    return scheme_false;
-  }
-
-  /* setPromiscuousMode(sock, index); */
-
-  return (Scheme_Object*) scheme_make_integer(sock);
-#endif
-
-#if defined(__APPLE__) && defined(__MACH__)
-  return scheme_false;
-#endif
+static Scheme_Object *create_and_bind_socket(int argc, Scheme_Object **argv) {
+  int sock;
+  char const *interfaceName = SCHEME_BYTE_STR_VAL(argv[0]);
+  sock = openSocket(interfaceName);
+  return (sock == -1) ? scheme_false : scheme_make_integer(sock);
 }
 
-Scheme_Object *close_socket(int argc, Scheme_Object **argv) {
-#ifdef __linux__
+static Scheme_Object *close_socket(int argc, Scheme_Object **argv) {
   int sock;
   sock = SCHEME_INT_VAL(argv[0]);
 
@@ -134,77 +255,80 @@ Scheme_Object *close_socket(int argc, Scheme_Object **argv) {
   }
 
   return scheme_true;
-#endif
-
-#if defined(__APPLE__) && defined(__MACH__)
-  return scheme_false;
-#endif
 }
 
-#ifdef __linux__
+static Scheme_Object *socket_read_buffer_length(int argc, Scheme_Object **argv) {
+  int sock;
+  sock = SCHEME_INT_VAL(argv[0]);
+  return scheme_make_integer(readBufferLength(sock));
+}
 
 struct ReadArgs {
   int sock;
   char *buf;
   int blen;
-  int *bytes_read;
+  int bytes_read;
 };
 
-void do_actual_read(void *read_args) {
-  struct ReadArgs *args = (struct ReadArgs *)read_args;
-  int bytes_read;
-  bytes_read = read(args->sock, args->buf, args->blen);
-  *(args->bytes_read) = bytes_read;
-  scheme_signal_received();
+static void *do_actual_read(void *read_args) {
+  struct ReadArgs *args = (struct ReadArgs *) read_args;
+  args->bytes_read = read(args->sock, args->buf, args->blen);
+  if (args->bytes_read == -1) {
+    perror("packet-socket read");
+  }
+  //scheme_signal_received();
+  return NULL;
 }
 
-int is_read_done(Scheme_Object *data) {
-  struct ReadArgs *read_args = (struct ReadArgs*)data;
-  return *(read_args->bytes_read) != 0;
+static int is_read_done(Scheme_Object *data) {
+  struct ReadArgs *read_args = (struct ReadArgs*) data;
+  return read_args->bytes_read != 0;
 }
 
-void prepare_for_sleep(Scheme_Object *data, void *fds) {
-  struct ReadArgs *read_args = (struct ReadArgs*)data;
+static void prepare_for_sleep(Scheme_Object *data, void *fds) {
+  struct ReadArgs *read_args = (struct ReadArgs*) data;
 
   MZ_FD_SET(read_args->sock, scheme_get_fdset(fds, 1));
 }
 
-#endif
-
 Scheme_Object *socket_read(int argc, Scheme_Object **argv) {
-#ifdef __linux__
-  struct ReadArgs *read_args;
-  int *bytes_read;
+  struct ReadArgs read_args;
   pthread_t read_thread;
-  int read_count;
+  Scheme_Object *result = scheme_null;
 
-  bytes_read = (int*)malloc(sizeof(int));
-  *bytes_read = 0;
+  read_args.sock = SCHEME_INT_VAL(argv[0]);
+  read_args.buf =  SCHEME_BYTE_STR_VAL(argv[1]);
+  read_args.blen = SCHEME_BYTE_STRLEN_VAL(argv[1]);
+  read_args.bytes_read = 0;
 
-  read_args = (struct ReadArgs*)malloc(sizeof(struct ReadArgs));
-  read_args->sock = SCHEME_INT_VAL(argv[0]);
-  read_args->buf =  SCHEME_BYTE_STR_VAL(argv[1]);
-  read_args->blen = SCHEME_BYTE_STRLEN_VAL(argv[1]);
-  read_args->bytes_read = bytes_read;
+  /* printf("original thread sock: %d, buf: %p, len: %d\n", */
+  /* 	 read_args.sock, */
+  /* 	 read_args.buf, */
+  /* 	 read_args.blen); */
+  /* fflush(NULL); */
 
-  //printf("original thread sock: %d, buf: %p\n", read_args->sock, read_args->buf);
-  pthread_create(&read_thread, NULL, do_actual_read, (void*)read_args);
+  pthread_create(&read_thread, NULL, do_actual_read, &read_args);
+  scheme_block_until(is_read_done, prepare_for_sleep, (Scheme_Object *) &read_args, -1);
 
-  scheme_block_until(is_read_done, prepare_for_sleep, (Scheme_Object*)read_args, -1);
+  {
+    int extractionState = 0;
+    int baseOffset = 0;
+    int length = 0;
 
-  read_count = *bytes_read;
-  free(read_args);
-  free(bytes_read);
-  return (Scheme_Object*)scheme_make_integer(read_count);
-#endif
+    do {
+      extractionState = extractPacket(read_args.buf, read_args.bytes_read, extractionState,
+				      &baseOffset,
+				      &length);
+      result = scheme_make_pair(scheme_make_pair(scheme_make_integer(baseOffset),
+						 scheme_make_integer(length)),
+				result);
+    } while (extractionState < read_args.bytes_read);
+  }
 
-#if defined(__APPLE__) && defined(__MACH__)
-  return scheme_false;
-#endif
+  return result;
 }
 
-Scheme_Object *socket_write(int argc, Scheme_Object **argv) {
-#ifdef __linux__
+static Scheme_Object *socket_write(int argc, Scheme_Object **argv) {
   int sock;
   char *buf;
   int blen, bytes_written;
@@ -213,34 +337,32 @@ Scheme_Object *socket_write(int argc, Scheme_Object **argv) {
   buf =  SCHEME_BYTE_STR_VAL(argv[1]);
   blen = SCHEME_BYTE_STRLEN_VAL(argv[1]);
   bytes_written = write(sock, buf, blen);
-  return (Scheme_Object*)scheme_make_integer(bytes_written);
-#endif
-
-#if defined(__APPLE__) && defined(__MACH__)
-  return scheme_false;
-#endif
+  return scheme_make_integer(bytes_written);
 }
 
 Scheme_Object *scheme_reload(Scheme_Env *env) {
   Scheme_Env *module_env;
-  Scheme_Object *proc, *proc2, *proc3, *read_proc, *write_proc;
+  Scheme_Object *proc;
 
   module_env = scheme_primitive_module(scheme_intern_symbol("packet-socket-extension"), env);
 
   proc = scheme_make_prim_w_arity(enumerate_interfaces, "enumerate-interfaces", 0, 0);
   scheme_add_global("enumerate-interfaces", proc, module_env);
 
-  proc2 = scheme_make_prim_w_arity(close_socket, "socket-close", 1, 1);
-  scheme_add_global("close-socket", proc2, module_env);
+  proc = scheme_make_prim_w_arity(close_socket, "socket-close", 1, 1);
+  scheme_add_global("socket-close", proc, module_env);
 
-  proc3 = scheme_make_prim_w_arity(create_and_bind_socket, "create-and-bind-socket", 1, 1);
-  scheme_add_global("create-and-bind-socket", proc3, module_env);
+  proc = scheme_make_prim_w_arity(create_and_bind_socket, "create-and-bind-socket", 1, 1);
+  scheme_add_global("create-and-bind-socket", proc, module_env);
 
-  read_proc = scheme_make_prim_w_arity(socket_read, "socket-read", 2, 2);
-  scheme_add_global("socket-read", read_proc, module_env);
+  proc = scheme_make_prim_w_arity(socket_read_buffer_length, "socket-read-buffer-length", 1, 1);
+  scheme_add_global("socket-read-buffer-length", proc, module_env);
 
-  write_proc = scheme_make_prim_w_arity(socket_write, "socket-write", 2, 2);
-  scheme_add_global("socket-write", write_proc, module_env);
+  proc = scheme_make_prim_w_arity(socket_read, "socket-read", 2, 2);
+  scheme_add_global("socket-read", proc, module_env);
+
+  proc = scheme_make_prim_w_arity(socket_write, "socket-write", 2, 2);
+  scheme_add_global("socket-write", proc, module_env);
 
   scheme_finish_primitive_module(module_env);
   return scheme_void;
