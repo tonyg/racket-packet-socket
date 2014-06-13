cb852a127171d843d1c88cf90ca6c1711c4403e8
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2014-06-13 19:50:56 -0400

    Improved example

--- example.rkt
+++ example.rkt
@@ -1,21 +1,32 @@
-#lang racket/base
+#lang racket
 
 (require packet-socket)
+(require (only-in file/sha1 bytes->hex-string))
 
-(define device-name "wlan0")
+(define device-names (raw-interface-names))
+(printf "Device names: ~a\n" device-names)
 
-(raw-interface-names)
+(when (null? device-names)
+  (error 'example "No available network devices."))
+
+(define device-name
+  (let loop ((candidates '("wlan0" "eth0" "en0" "en1")))
+    (match candidates
+      ['() (car device-names)] ;; whatever is left
+      [(cons candidate rest)
+       (if (member candidate device-names)
+	   candidate
+	   (loop rest))])))
 
 (define h (raw-interface-open device-name))
-h
 (when (not h)
   (error 'example "Couldn't open device ~v" device-name))
+(printf "Opened device ~a, yielding handle ~v\n" device-name h)
 
 (let loop ()
-  (define p (raw-interface-read h))
-  (write p)
-  (newline)
   (flush-output)
+  (define p (raw-interface-read h))
+  (printf "PACKET: ~a\n" (bytes->hex-string p))
   (loop))
 
 ;; (raw-interface-close h)
